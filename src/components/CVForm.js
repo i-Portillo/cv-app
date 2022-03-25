e2081c6561cd949d8c8f5de8f6c750d36a12f65e
iPortillo <ismaelpa91@gmail.com>
2022-03-25 14:54:39 +0100

    Refactor and restructure of data

--- src/components/CVForm.js
+++ src/components/CVForm.js
@@ -1,76 +1,12 @@
 import React, { Component } from 'react';
 import FieldSet from './FieldSet';
-import uniqid from 'uniqid';
 
 class CVForm extends Component {
     constructor(props) {
         super(props);
 
-        this.forms = [
-            {
-                title: "Personal",
-                fields: [
-                    {
-                        name: "First Name",
-                        type: "text",
-                        id: uniqid(),
-                    },
-                    {
-                        name: "Last Name",
-                        type: "text",
-                        id: uniqid(),
-                    },
-                    {
-                        name: "Title",
-                        type: "text",
-                        id: uniqid(),
-                    },
-                ],
-                id: uniqid(),
-            },
-            {
-                title: "Experience",
-                fields: [
-                    {
-                        name: "Job Title",
-                        type: "text",
-                        id: uniqid(),
-                    },
-                    {
-                        name: "Job Description",
-                        type: "textarea",
-                        id: uniqid(),
-                    },
-                    {
-                        name: "Date",
-                        type: "date",
-                        id: uniqid(),
-                    },
-                ],
-                id: uniqid(),
-            },
-            {
-                title: "Experience",
-                fields: [
-                    {
-                        name: "Job Title",
-                        type: "text",
-                        id: uniqid(),
-                    },
-                    {
-                        name: "Job Description",
-                        type: "textarea",
-                        id: uniqid(),
-                    },
-                    {
-                        name: "Date",
-                        type: "date-range",
-                        id: uniqid(),
-                    },
-                ],
-                id: uniqid(),
-            }
-        ]
+        this.forms = props.forms;
+
     }
 
     render() {
